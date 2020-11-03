e2b042ec865234d2e061ca78ab5c2690eb1ff794
Lukasz Malinowski <lukasz.malinowski@uniteam.pl>
2020-11-03 16:23:55 +0100

    first commit

---
+++ src/main/java/pl/lukaszmalina/tydzien2/Tydzien2Application.java
@@ -0,0 +1,13 @@
+package pl.lukaszmalina.tydzien2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tydzien2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tydzien2Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/lukaszmalina/tydzien2/Tydzien2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lukaszmalina.tydzien2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tydzien2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
